2d2a6f6ed941dd6c57ba8ad7613e9fc9b785ab79
Francisco Politi <ppoliti8@gmail.com>
2020-08-11 18:43:45 +0200

    Updates

---
+++ precache-manifest.99ae7ae36d8447a80fd62284e40fe657.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae8c8704c8d9f7297696ab05a8fd4ec",
+    "url": "/meditation-timer/index.html"
+  },
+  {
+    "revision": "de3a4676d3aa765da355",
+    "url": "/meditation-timer/static/css/main.4e56a85c.chunk.css"
+  },
+  {
+    "revision": "90f8f00b0c9f7c0eee2c",
+    "url": "/meditation-timer/static/js/2.81cef546.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/meditation-timer/static/js/2.81cef546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de3a4676d3aa765da355",
+    "url": "/meditation-timer/static/js/main.00e78fd6.chunk.js"
+  },
+  {
+    "revision": "e5cf40a7bb9502fd40a6",
+    "url": "/meditation-timer/static/js/runtime-main.29ab67ca.js"
+  },
+  {
+    "revision": "68b6036661d83480212a7d761c03e184",
+    "url": "/meditation-timer/static/media/bell4.68b60366.mp3"
+  },
+  {
+    "revision": "2e1feb19dc52ffd4ae6dd8dbe2f9b91e",
+    "url": "/meditation-timer/static/media/bowl-struck.2e1feb19.mp3"
+  },
+  {
+    "revision": "fa1328fcdbb170ceca6f1f2d5581785d",
+    "url": "/meditation-timer/static/media/buddhist-prayer-bell.fa1328fc.mp3"
+  },
+  {
+    "revision": "9f07a3df80b5eea9cfdcf9616e3d03d5",
+    "url": "/meditation-timer/static/media/cog-solid.9f07a3df.svg"
+  },
+  {
+    "revision": "368c1978e0ee43c7af5f5d00d91f7d0a",
+    "url": "/meditation-timer/static/media/gong-burmese.368c1978.wav"
+  },
+  {
+    "revision": "b8a8b3be58a2b8744c1ff0a577d93f46",
+    "url": "/meditation-timer/static/media/gong-chinese.b8a8b3be.wav"
+  },
+  {
+    "revision": "f3aa308a047a496163bdf002c5551b24",
+    "url": "/meditation-timer/static/media/long-bell.f3aa308a.mp3"
+  },
+  {
+    "revision": "760cc3b2272584f445867de0dc08e2ce",
+    "url": "/meditation-timer/static/media/tingsha.760cc3b2.wav"
+  },
+  {
+    "revision": "85f7bec3c815e2b4d22ef62c7a705707",
+    "url": "/meditation-timer/static/media/zen-bell.85f7bec3.mp3"
+  }
+]);
